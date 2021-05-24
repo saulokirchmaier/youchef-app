7b9075c7dc9893b22d19dfd3d407bde56d030fcd
saulokirchmaier <saulokirchmaier@gmail.com>
2021-05-24 12:36:48 -0300

    explorar e shadow card css

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,17 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import './Styles/Explore.css';
 
 function Explore() {
   return (
     <div>
       <Header title="Explorar" />
-      <Link to="/explorar/comidas">
-        <button type="button" data-testid="explore-food">Explorar Comidas</button>
-      </Link>
-      <Link to="/explorar/bebidas">
-        <button type="button" data-testid="explore-drinks">Explorar Bebidas</button>
-      </Link>
+      <div className="Explore">
+        <Link to="/explorar/comidas">
+          <button
+            className="button"
+            type="button"
+            data-testid="explore-food"
+          >
+            Explorar Comidas
+          </button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <button
+            className="button"
+            type="button"
+            data-testid="explore-drinks"
+          >
+            Explorar Bebidas
+          </button>
+        </Link>
+      </div>
       <Footer />
     </div>
   );
