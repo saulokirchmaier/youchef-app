4f4a68f68d63593869f454af3b6824bfe3eb9a30
saulokirchmaier <saulokirchmaier@gmail.com>
2021-06-10 11:22:12 -0300

    Updates

---
+++ precache-manifest.8b039e257b46f416e99aac818c0c077c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb7d2286db4ac0da4796657e4c0e59f",
+    "url": "/youchef-app/index.html"
+  },
+  {
+    "revision": "ae42777cd0636f96ffe8",
+    "url": "/youchef-app/static/css/2.68d715af.chunk.css"
+  },
+  {
+    "revision": "1b07e15c01df57b4ea00",
+    "url": "/youchef-app/static/css/main.920799ce.chunk.css"
+  },
+  {
+    "revision": "ae42777cd0636f96ffe8",
+    "url": "/youchef-app/static/js/2.28f6ee60.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/youchef-app/static/js/2.28f6ee60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b07e15c01df57b4ea00",
+    "url": "/youchef-app/static/js/main.13983cc5.chunk.js"
+  },
+  {
+    "revision": "cc013831504882564df4",
+    "url": "/youchef-app/static/js/runtime-main.18d6d8eb.js"
+  },
+  {
+    "revision": "751d2a055dd4253023183b3a9e32d272",
+    "url": "/youchef-app/static/media/YouChef.751d2a05.png"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/youchef-app/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/youchef-app/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/youchef-app/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/youchef-app/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/youchef-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/youchef-app/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/youchef-app/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/youchef-app/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/youchef-app/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/youchef-app/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/youchef-app/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
